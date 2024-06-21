000af48d921af2866bb939be7dbd4d51f852ab8f
ayaksoy <yasir.aksoy@outlook.com>
2024-06-21 16:19:11 +0300

    Book Controller

--- WebApi/obj/Debug/net6.0/WebApiExample.AssemblyInfo.cs
+++ WebApi/obj/Debug/net6.0/WebApiExample.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApiExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a692a6728c6d0f1da5b5e1f8d3f6ecd79170f56e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApiExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApiExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
